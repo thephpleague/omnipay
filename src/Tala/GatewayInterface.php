9b20ceb481e0c7b5b52f05537c4a632982c51b93
Alexander Deruwe <alexander.deruwe@gmail.com>
2013-01-16 21:44:45 +0100

    Improve phpdoc

--- src/Tala/GatewayInterface.php
+++ src/Tala/GatewayInterface.php
@@ -11,15 +11,21 @@
 
 namespace Tala;
 
+use BadMethodCallException;
 use Tala\Request;
 
 /**
  * Payment gateway interface
+ *
+ * @author Adrian Macneil <adrian@adrianmacneil.com>
+ * @author Alexander Deruwe <alexander.deruwe@gmail.com>
  */
 interface GatewayInterface
 {
     /**
      * Initialize the gateway with an associative array of settings.
+     *
+     * @param array $settings Associative array of settings
      */
     public function initialize($settings);
 
@@ -30,26 +36,58 @@ interface GatewayInterface
 
     /**
      * Authorizes a new payment.
+     *
+     * @param Request $request Payment request
+     * @param mixed   $source  Source
+     *
+     * @throws BadMethodCallException If the operation is not supported
+     *
+     * @return Response
      */
     public function authorize(Request $request, $source);
 
     /**
      * Handles return from an authorization.
+     *
+     * @param Request $request Payment request
+     *
+     * @throws BadMethodCallException If the operation is not supported
+     *
+     * @return Response
      */
     public function completeAuthorize(Request $request);
 
     /**
      * Capture an authorized payment.
+     *
+     * @param Request $request Payment request
+     *
+     * @throws BadMethodCallException If the operation is not supported
+     *
+     * @return Response
      */
     public function capture(Request $request);
 
     /**
      * Creates a new charge (combined authorize + capture).
+     *
+     * @param Request $request Payment request
+     * @param mixed   $source  Source
+     *
+     * @throws BadMethodCallException If the operation is not supported
+     *
+     * @return Response
      */
     public function purchase(Request $request, $source);
 
     /**
      * Handle return from a purchase.
+     *
+     * @param Request $request Payment request
+     *
+     * @throws BadMethodCallException If the operation is not supported
+     *
+     * @return Response
      */
     public function completePurchase(Request $request);
 
@@ -57,6 +95,12 @@ interface GatewayInterface
      * Refund an existing transaction.
      * Generally this will refund a transaction which has been already submitted for processing,
      * and may be called up to 30 days after submitting the transaction.
+     *
+     * @param Request $request Payment request
+     *
+     * @throws BadMethodCallException If the operation is not supported
+     *
+     * @return Response
      */
     public function refund(Request $request);
 
@@ -64,6 +108,12 @@ interface GatewayInterface
      * Void an existing transaction.
      * Generally this will prevent the transaction from being submitted for processing,
      * and can only be called up to 24 hours after submitting the transaction.
+     *
+     * @param Request $request Payment request
+     *
+     * @throws BadMethodCallException If the operation is not supported
+     *
+     * @return Response
      */
     public function void(Request $request);
 }
