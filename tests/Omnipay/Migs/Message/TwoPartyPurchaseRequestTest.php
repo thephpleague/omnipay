52ea33e749e4d0c7dcd2078b0c4ddbd9bd68b4a7
Adrian Macneil <adrian@adrianmacneil.com>
2013-06-01 18:47:55 +1000

    Fix Migs gateway tests

--- tests/Omnipay/Migs/Message/TwoPartyPurchaseRequestTest.php
+++ tests/Omnipay/Migs/Message/TwoPartyPurchaseRequestTest.php
@@ -21,30 +21,27 @@ class TwoPartyPurchaseRequestTest extends TestCase
         $this->request = new TwoPartyPurchaseRequest($this->getHttpClient(), $this->getHttpRequest());
     }
 
-    public function testSignature()
+    public function testCalculateHash()
     {
-        $this->request->initialize(
-            array(
-                'amount' => 1200,
-                'transactionId' => 123,
-                'card' => new CreditCard(
-                    array(
-                        'number' => '4111111111111111',
-                        'expiryMonth' => '05',
-                        'expiryYear' => '2013',
-                        'cvv' => '123',
-                    )
-                ),
-                'merchantId'                   => '123',
-                'merchantAccessCode'           => '123',
-                'secureHash'                   => '123',
-                'returnUrl' => 'https://www.example.com/return'
-            )
+        $data = array(
+            'vpc_Merchant' => '123',
+            'vpc_AccessCode' => '123',
+            'vpc_Version' => '1',
+            'vpc_Locale' => 'en',
+            'vpc_Command' => 'pay',
+            'vpc_Amount' => '1200',
+            'vpc_MerchTxnRef' => '123',
+            'vpc_OrderInfo' => '',
+            'vpc_ReturnURL' => 'https://www.example.com/return',
+            'vpc_CardNum' => '4111111111111111',
+            'vpc_CardExp' => '1305',
+            'vpc_CardSecurityCode' => '123',
         );
 
-        $data = $this->request->getData();
+        $this->request->setSecureHash('123');
+        $hash = $this->request->calculateHash($data);
 
-        $this->assertSame('2624B4BABED7CCA98665238D75560600', $data['vpc_SecureHash']);
+        $this->assertSame('2624B4BABED7CCA98665238D75560600', $hash);
     }
 
     public function testPurchase()
@@ -55,14 +52,7 @@ class TwoPartyPurchaseRequestTest extends TestCase
             array(
                 'amount' => 1200,
                 'transactionId' => 123,
-                'card' => new CreditCard(
-                    array(
-                        'number' => '4111111111111111',
-                        'expiryMonth' => '05',
-                        'expiryYear' => '2013',
-                        'cvv' => '123',
-                    )
-                ),
+                'card' => $this->getValidCard(),
                 'merchantId'                   => '123',
                 'merchantAccessCode'           => '123',
                 'secureHash'                   => '123',
