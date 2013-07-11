befec36beff680093176311cc3a522c7697d505b
Boris Schapira <bschapira@gmail.com>
2013-07-11 18:43:04 +0200

    Init. Sips Gateway by cloning the Dummy

---
+++ tests/Omnipay/Sips/GatewayTest.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of the Omnipay package.
+ *
+ * (c) Adrian Macneil <adrian@adrianmacneil.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Omnipay\Sips;
+
+use Omnipay\GatewayTestCase;
+
+class GatewayTest extends GatewayTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
+
+        $this->options = array(
+            'amount' => '10.00',
+            'card' => $this->getValidCard(),
+        );
+    }
+
+    public function testAuthorizeSuccess()
+    {
+        // card numbers ending in even number should be successful
+        $this->options['card']['number'] = '4242424242424242';
+        $response = $this->gateway->authorize($this->options)->send();
+
+        $this->assertInstanceOf('\Omnipay\Sips\Message\Response', $response);
+        $this->assertTrue($response->isSuccessful());
+        $this->assertFalse($response->isRedirect());
+        $this->assertNotEmpty($response->getTransactionReference());
+        $this->assertSame('Success', $response->getMessage());
+    }
+
+    public function testAuthorizeFailure()
+    {
+        // card numbers ending in odd number should be declined
+        $this->options['card']['number'] = '4111111111111111';
+        $response = $this->gateway->authorize($this->options)->send();
+
+        $this->assertInstanceOf('\Omnipay\Sips\Message\Response', $response);
+        $this->assertFalse($response->isSuccessful());
+        $this->assertFalse($response->isRedirect());
+        $this->assertNotEmpty($response->getTransactionReference());
+        $this->assertSame('Failure', $response->getMessage());
+    }
+
+    public function testPurchaseSuccess()
+    {
+        // card numbers ending in even number should be successful
+        $this->options['card']['number'] = '4242424242424242';
+        $response = $this->gateway->purchase($this->options)->send();
+
+        $this->assertInstanceOf('\Omnipay\Sips\Message\Response', $response);
+        $this->assertTrue($response->isSuccessful());
+        $this->assertFalse($response->isRedirect());
+        $this->assertNotEmpty($response->getTransactionReference());
+        $this->assertSame('Success', $response->getMessage());
+    }
+
+    public function testPurcahseFailure()
+    {
+        // card numbers ending in odd number should be declined
+        $this->options['card']['number'] = '4111111111111111';
+        $response = $this->gateway->purchase($this->options)->send();
+
+        $this->assertInstanceOf('\Omnipay\Sips\Message\Response', $response);
+        $this->assertFalse($response->isSuccessful());
+        $this->assertFalse($response->isRedirect());
+        $this->assertNotEmpty($response->getTransactionReference());
+        $this->assertSame('Failure', $response->getMessage());
+    }
+}
