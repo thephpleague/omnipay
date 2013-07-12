1e819412c1c388603902345af54f3f56912f292d
Alexander Deruwe <alexander.deruwe@gmail.com>
2013-07-12 11:35:34 +0200

    Add all remaining fields to PurchaseRequest

--- tests/Omnipay/MultiSafepay/Message/PurchaseRequestTest.php
+++ tests/Omnipay/MultiSafepay/Message/PurchaseRequestTest.php
@@ -28,22 +28,55 @@ class PurchaseRequestTest extends TestCase
             'accountId' => '111111',
             'siteId' => '222222',
             'siteCode' => '333333',
+            'notifyUrl' => 'http://localhost/notify',
+            'cancelUrl' => 'http://localhost/cancel',
+            'returnUrl' => 'http://localhost/return',
+            'gateway' => 'IDEAL',
+            'issuer' => 'issuer',
             'transactionId' => '123456',
             'currency' => 'EUR',
             'amount' => '100.00',
             'description' => 'desc',
+            'extraData1' => 'extra 1',
+            'extraData2' => 'extra 2',
+            'extraData3' => 'extra 3',
+            'language' => 'a language',
             'clientIp' => '127.0.0.1',
+            'googleAnalyticsCode' => 'analytics code',
             'card' => array(
                 'email' => 'something@example.com',
+                'firstName' => 'first name',
+                'lastName' => 'last name',
+                'address1' => 'address 1',
+                'address2' => 'address 2',
+                'postcode' => '1000',
+                'city' => 'a city',
+                'country' => 'a country',
+                'phone' => 'phone number',
             )
         ));
     }
 
     /**
-     * @dataProvider dataProvider
+     * @dataProvider allDataProvider
      */
     public function testGetData($xml)
     {
+        $data = $this->request->getData();
+        $this->assertInstanceOf('SimpleXMLElement', $data);
+
+        // Just so the provider remains readable...
+        $dom = dom_import_simplexml($data)->ownerDocument;
+        $dom->formatOutput = true;
+        $this->assertEquals($xml, $dom->saveXML());
+    }
+
+    /**
+     * @dataProvider noIssuerDataProvider
+     */
+    public function testGetDataWithNonIDEALGatewayDoesNotSetIssuer($xml)
+    {
+        $this->request->setGateway('another');
         $data = $this->request->getData();
         $this->assertInstanceOf('SimpleXMLElement', $data);
 
@@ -65,7 +98,7 @@ class PurchaseRequestTest extends TestCase
         $this->assertEquals('bb886caff589f17e81b21097a39e47c2', $signature);
     }
 
-    public function dataProvider()
+    public function allDataProvider()
     {
         $xml = <<<EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -74,16 +107,83 @@ class PurchaseRequestTest extends TestCase
     <account>111111</account>
     <site_id>222222</site_id>
     <site_secure_code>333333</site_secure_code>
+    <notification_url>http://localhost/notify</notification_url>
+    <cancel_url>http://localhost/cancel</cancel_url>
+    <redirect_url>http://localhost/return</redirect_url>
+    <gateway>IDEAL</gateway>
   </merchant>
+  <gatewayinfo>
+    <issuerid>issuer</issuerid>
+  </gatewayinfo>
   <customer>
     <ipaddress>127.0.0.1</ipaddress>
+    <locale>a language</locale>
     <email>something@example.com</email>
+    <firstname>first name</firstname>
+    <lastname>last name</lastname>
+    <address1>address 1</address1>
+    <address2>address 2</address2>
+    <zipcode>1000</zipcode>
+    <city>a city</city>
+    <country>a country</country>
+    <phone>phone number</phone>
   </customer>
+  <google_analytics>analytics code</google_analytics>
   <transaction>
     <id>123456</id>
     <currency>EUR</currency>
     <amount>10000</amount>
     <description>desc</description>
+    <var1>extra 1</var1>
+    <var2>extra 2</var2>
+    <var3>extra 3</var3>
+  </transaction>
+  <signature>bb886caff589f17e81b21097a39e47c2</signature>
+</redirecttransaction>
+
+EOF;
+
+        return array(
+            array($xml),
+        );
+    }
+
+    public function noIssuerDataProvider()
+    {
+        $xml = <<<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<redirecttransaction ua="Omnipay">
+  <merchant>
+    <account>111111</account>
+    <site_id>222222</site_id>
+    <site_secure_code>333333</site_secure_code>
+    <notification_url>http://localhost/notify</notification_url>
+    <cancel_url>http://localhost/cancel</cancel_url>
+    <redirect_url>http://localhost/return</redirect_url>
+    <gateway>another</gateway>
+  </merchant>
+  <customer>
+    <ipaddress>127.0.0.1</ipaddress>
+    <locale>a language</locale>
+    <email>something@example.com</email>
+    <firstname>first name</firstname>
+    <lastname>last name</lastname>
+    <address1>address 1</address1>
+    <address2>address 2</address2>
+    <zipcode>1000</zipcode>
+    <city>a city</city>
+    <country>a country</country>
+    <phone>phone number</phone>
+  </customer>
+  <google_analytics>analytics code</google_analytics>
+  <transaction>
+    <id>123456</id>
+    <currency>EUR</currency>
+    <amount>10000</amount>
+    <description>desc</description>
+    <var1>extra 1</var1>
+    <var2>extra 2</var2>
+    <var3>extra 3</var3>
   </transaction>
   <signature>bb886caff589f17e81b21097a39e47c2</signature>
 </redirecttransaction>
