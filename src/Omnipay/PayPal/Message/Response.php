0982dd85429a69a562f14b7ec718228bf613668a
lfjeff <jeff@jasminezone.com>
2013-08-06 13:31:39 -0700

    - added getCardReference() - added getFullMessage()

--- src/Omnipay/PayPal/Message/Response.php
+++ src/Omnipay/PayPal/Message/Response.php
@@ -39,8 +39,39 @@ class Response extends AbstractResponse
         }
     }
 
+    public function getCardReference()
+    {
+        return isset($this->data['TRANSACTIONID']) ? $this->data['TRANSACTIONID'] : null;
+    }
+
     public function getMessage()
     {
         return isset($this->data['L_LONGMESSAGE0']) ? $this->data['L_LONGMESSAGE0'] : null;
+    }
+
+    public function getFullMessage()
+    {
+        $msg = '';
+        $lmsg = '';
+
+        // L_LONGMESSAGE0 often says "This transaction cannot be processed",
+        // which does not give much information about why the transaction
+        // failed.  To give a more complete picture, combine both messages
+        // to return a message like:
+        //   "Processor Decline|This transaction cannot be processed"
+        if (isset($this->data['L_SHORTMESSAGE0']) && $this->data['L_SHORTMESSAGE0']) {
+            $msg = $this->data['L_SHORTMESSAGE0'];
+        }
+
+        if (isset($this->data['L_LONGMESSAGE0']) && $this->data['L_LONGMESSAGE0']) {
+            $lmsg = $this->data['L_LONGMESSAGE0'];
+        }
+
+        if ($msg || $lmsg) {
+            $msg .= "|$lmsg";
+            return $msg;
+        }
+
+        return null;
     }
 }
