3d63888372db7fb6adc0f97ff50c4ea55ceec052
Shane O'Connor <shane@soconnor.co.uk>
2013-10-29 09:36:14 -0700

    Adding Request Handlers for Sagepay Direct Token registration

---
+++ src/Omnipay/SagePay/Message/DirectCreateTokenRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Omnipay\SagePay\Message;
+
+/**
+ * Sage Pay Direct Purchase Request
+ */
+class DirectCreateTokenRequest extends DirectAuthorizeRequest
+{
+    protected $action = 'TOKEN';
+}
