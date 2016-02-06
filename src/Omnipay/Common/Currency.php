af429996b24cb8543d057c05841b2205208af98e
Barry vd. Heuvel <barry@fruitcakestudio.nl>
2016-02-07 00:25:20 +0100

    Use alcohol/4217 currency list

--- src/Omnipay/Common/Currency.php
+++ src/Omnipay/Common/Currency.php
@@ -5,6 +5,8 @@
 
 namespace Omnipay\Common;
 
+use Alcohol\ISO4217;
+
 /**
  * Currency class
  *
@@ -66,12 +68,15 @@ class Currency
      */
     public static function find($code)
     {
-        $code = strtoupper($code);
-        $currencies = static::all();
+        $iso4217 = new ISO4217();
 
-        if (isset($currencies[$code])) {
-            return new static($code, $currencies[$code]['numeric'], $currencies[$code]['decimals']);
+        try {
+            $currency = $iso4217->getByCode($code);
+        } catch (\OutOfBoundsException $e) {
+            return null;
         }
+
+        return new static($currency['alpha3'], $currency['numeric'], $currency['exp']);
     }
 
     /**
@@ -81,56 +86,16 @@ class Currency
      */
     public static function all()
     {
-        return array(
-            'ARS' => array('numeric' => '032', 'decimals' => 2),
-            'AUD' => array('numeric' => '036', 'decimals' => 2),
-            'BOB' => array('numeric' => '068', 'decimals' => 2),
-            'BRL' => array('numeric' => '986', 'decimals' => 2),
-            'BTC' => array('numeric' => null, 'decimals' => 8),
-            'CAD' => array('numeric' => '124', 'decimals' => 2),
-            'CHF' => array('numeric' => '756', 'decimals' => 2),
-            'CLP' => array('numeric' => '152', 'decimals' => 0),
-            'CNY' => array('numeric' => '156', 'decimals' => 2),
-            'COP' => array('numeric' => '170', 'decimals' => 2),
-            'CRC' => array('numeric' => '188', 'decimals' => 2),
-            'CZK' => array('numeric' => '203', 'decimals' => 2),
-            'DKK' => array('numeric' => '208', 'decimals' => 2),
-            'DOP' => array('numeric' => '214', 'decimals' => 2),
-            'EUR' => array('numeric' => '978', 'decimals' => 2),
-            'FJD' => array('numeric' => '242', 'decimals' => 2),
-            'GBP' => array('numeric' => '826', 'decimals' => 2),
-            'GTQ' => array('numeric' => '320', 'decimals' => 2),
-            'HKD' => array('numeric' => '344', 'decimals' => 2),
-            'HUF' => array('numeric' => '348', 'decimals' => 2),
-            'ILS' => array('numeric' => '376', 'decimals' => 2),
-            'INR' => array('numeric' => '356', 'decimals' => 2),
-            'JPY' => array('numeric' => '392', 'decimals' => 0),
-            'KRW' => array('numeric' => '410', 'decimals' => 0),
-            'LAK' => array('numeric' => '418', 'decimals' => 0),
-            'MXN' => array('numeric' => '484', 'decimals' => 2),
-            'MYR' => array('numeric' => '458', 'decimals' => 2),
-            'NOK' => array('numeric' => '578', 'decimals' => 2),
-            'NZD' => array('numeric' => '554', 'decimals' => 2),
-            'OMR' => array('numeric' => '512', 'decimals' => 3),
-            'PEN' => array('numeric' => '604', 'decimals' => 2),
-            'PGK' => array('numeric' => '598', 'decimals' => 2),
-            'PHP' => array('numeric' => '608', 'decimals' => 2),
-            'PLN' => array('numeric' => '985', 'decimals' => 2),
-            'PYG' => array('numeric' => '600', 'decimals' => 0),
-            'SBD' => array('numeric' => '090', 'decimals' => 2),
-            'SEK' => array('numeric' => '752', 'decimals' => 2),
-            'SGD' => array('numeric' => '702', 'decimals' => 2),
-            'THB' => array('numeric' => '764', 'decimals' => 2),
-            'TOP' => array('numeric' => '776', 'decimals' => 2),
-            'TRY' => array('numeric' => '949', 'decimals' => 2),
-            'TWD' => array('numeric' => '901', 'decimals' => 2),
-            'USD' => array('numeric' => '840', 'decimals' => 2),
-            'UYU' => array('numeric' => '858', 'decimals' => 2),
-            'VEF' => array('numeric' => '937', 'decimals' => 2),
-            'VND' => array('numeric' => '704', 'decimals' => 0),
-            'VUV' => array('numeric' => '548', 'decimals' => 0),
-            'WST' => array('numeric' => '882', 'decimals' => 2),
-            'ZAR' => array('numeric' => '710', 'decimals' => 2),
-        );
+        $currencies = [];
+
+        $iso4217 = new ISO4217();
+        foreach ($iso4217->getAll() as $currency) {
+            $currencies[$currency['alpha3']] = [
+                'numeric' => $currency['numeric'],
+                'decimals' => $currency['exp'],
+            ];
+        }
+
+        return $currencies;
     }
 }
