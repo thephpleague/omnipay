b5420cc0502b1ebc2898324d6943f61711f6bf06
Barry vd. Heuvel <barry@fruitcakestudio.nl>
2016-02-13 23:53:34 +0100

    Use local namespace

--- src/Common/GatewayInterface.php
+++ src/Common/GatewayInterface.php
@@ -13,25 +13,25 @@ namespace League\Omnipay\Common;
  *
  * @see AbstractGateway
  *
- * @method \League\Omnipay\Common\Message\ResponseInterface authorize(array $options = array())         (Optional method)
+ * @method Message\ResponseInterface authorize(array $options = array())         (Optional method)
  *         Authorize an amount on the customers card
- * @method \League\Omnipay\Common\Message\ResponseInterface completeAuthorize(array $options = array()) (Optional method)
+ * @method Message\ResponseInterface completeAuthorize(array $options = array()) (Optional method)
  *         Handle return from off-site gateways after authorization
- * @method \League\Omnipay\Common\Message\ResponseInterface capture(array $options = array())           (Optional method)
+ * @method Message\ResponseInterface capture(array $options = array())           (Optional method)
  *         Capture an amount you have previously authorized
- * @method \League\Omnipay\Common\Message\ResponseInterface purchase(array $options = array())          (Optional method)
+ * @method Message\ResponseInterface purchase(array $options = array())          (Optional method)
  *         Authorize and immediately capture an amount on the customers card
- * @method \League\Omnipay\Common\Message\ResponseInterface completePurchase(array $options = array())  (Optional method)
+ * @method Message\ResponseInterface completePurchase(array $options = array())  (Optional method)
  *         Handle return from off-site gateways after purchase
- * @method \League\Omnipay\Common\Message\ResponseInterface refund(array $options = array())            (Optional method)
+ * @method Message\ResponseInterface refund(array $options = array())            (Optional method)
  *         Refund an already processed transaction
- * @method \League\Omnipay\Common\Message\ResponseInterface void(array $options = array())              (Optional method)
+ * @method Message\ResponseInterface void(array $options = array())              (Optional method)
  *         Generally can only be called up to 24 hours after submitting a transaction
- * @method \League\Omnipay\Common\Message\ResponseInterface createCard(array $options = array())        (Optional method)
+ * @method Message\ResponseInterface createCard(array $options = array())        (Optional method)
  *         The returned response object includes a cardReference, which can be used for future transactions
- * @method \League\Omnipay\Common\Message\ResponseInterface updateCard(array $options = array())        (Optional method)
+ * @method Message\ResponseInterface updateCard(array $options = array())        (Optional method)
  *         Update a stored card
- * @method \League\Omnipay\Common\Message\ResponseInterface deleteCard(array $options = array())        (Optional method)
+ * @method Message\ResponseInterface deleteCard(array $options = array())        (Optional method)
  *         Delete a stored card
 */
 interface GatewayInterface
